a51ad35b657515c851595201276208e04a0f90b3
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2025-02-28 09:05:43 +0100

    wip

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,168 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 5.2753445007940165,
+          "misMatchPercentage": "5.28",
+          "analysisTime": 466
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 12.10577584914404,
+          "misMatchPercentage": "12.11",
+          "analysisTime": 303
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/",
+        "expect": 0,
+        "viewportLabel": "mobile",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 66
+          },
+          "rawMisMatchPercentage": 14.102461743180307,
+          "misMatchPercentage": "14.10",
+          "analysisTime": 167
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/?test=1",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/?test=1",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 5.2753445007940165,
+          "misMatchPercentage": "5.28",
+          "analysisTime": 434
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/?test=1",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/?test=1",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 12.10577584914404,
+          "misMatchPercentage": "12.11",
+          "analysisTime": 349
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_1_tablet.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "test": "..\\bitmaps_test\\20250227-185115\\backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "selector": "document",
+        "fileName": "backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png",
+        "label": "home_page - komputronik_pl home_page navigation_bar no_html_cache",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://komputronikPl-selenium.alfa.netcorner.pl/?test=1",
+        "referenceUrl": "https://komputronikPl-sztos.alfa.netcorner.pl/?test=1",
+        "expect": 0,
+        "viewportLabel": "mobile",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 66
+          },
+          "rawMisMatchPercentage": 14.102461743180307,
+          "misMatchPercentage": "14.10",
+          "analysisTime": 181
+        },
+        "diffImage": "..\\bitmaps_test\\20250227-185115\\failed_diff_backstop_default_home_page_-_komputronik_pl_home_page_navigation_bar_no_html_cache_0_document_2_mobile.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
