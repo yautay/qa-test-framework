3ddb0dd308631d15b945053d200e5b83507584ec
m.pielaszkiewicz <m.pielaszkiewicz@komputronik.pl>
2025-11-25 16:32:22 +0100

    feat: add new test cases for komputronik-pl product pages with and without HTML cache

---
+++ tests/komputronik-pl/tests/test-product-pages/tests-rwd-front-products/test-product-page-54244125.js
@@ -0,0 +1,24 @@
+// tests/komputronik-pl/tests/test-product-pages/tests-rwd-front-products/test-product-page-54244125.js
+
+module.exports = {
+  scenarios: [
+    {
+      label: 'komputronik-pl product-page tests-rwd-front-products html-cache 54244125',
+      resource: "/product/54244125",
+      delay: 3000,
+      misMatchThreshold: 1,
+      requireSameDimensions: true,
+      report: ['browser'],
+      onReadyScript: './../../tests/komputronik-pl/scripts/close-cookies.js'
+    },
+    {
+      label: 'komputronik-pl product-page tests-rwd-front-products no-html-cache 54244125',
+      resource: '/product/54244125?test=1',
+      delay: 3000,
+      misMatchThreshold: 1,
+      requireSameDimensions: true,
+      report: ['browser'],
+      onReadyScript: './../../tests/komputronik-pl/scripts/close-cookies.js'
+    }
+  ]
+};
---
+++ tests/komputronik-pl/tests/test-product-pages/tests-rwd-front-products/test-product-page-54244129.js
@@ -0,0 +1,24 @@
+// tests/komputronik-pl/tests/test-product-pages/tests-rwd-front-products/test-product-page-54244129.js
+
+module.exports = {
+  scenarios: [
+    {
+      label: 'komputronik-pl product-page tests-rwd-front-products html-cache 54244129',
+      resource: "/product/54244129",
+      delay: 3000,
+      misMatchThreshold: 1,
+      requireSameDimensions: true,
+      report: ['browser'],
+      onReadyScript: './../../tests/komputronik-pl/scripts/close-cookies.js'
+    },
+    {
+      label: 'komputronik-pl product-page tests-rwd-front-products no-html-cache 54244129',
+      resource: '/product/54244129?test=1',
+      delay: 3000,
+      misMatchThreshold: 1,
+      requireSameDimensions: true,
+      report: ['browser'],
+      onReadyScript: './../../tests/komputronik-pl/scripts/close-cookies.js'
+    }
+  ]
+};
