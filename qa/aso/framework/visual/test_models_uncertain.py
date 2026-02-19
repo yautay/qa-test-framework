ad4832cf30db4fc5893cabbf3e22035b4d3de742
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2026-02-19 16:01:05 +0100

    fix: remove invalid to_dict test for VisualThresholds

--- qa/aso/framework/visual/test_models_uncertain.py
+++ qa/aso/framework/visual/test_models_uncertain.py
@@ -54,18 +54,3 @@ class TestVisualThresholdsFromDict:
         t = VisualThresholds.from_dict(d)
         assert t.pixel_uncertain_delta is None
         assert t.lpips_uncertain_delta is None
-
-
-class TestVisualThresholdsToDict:
-    def test_includes_uncertain_deltas(self):
-        t = VisualThresholds(
-            pixel_max=0.005,
-            lpips_max=0.08,
-            dists_max=0.08,
-            pixel_uncertain_delta=0.002,
-            lpips_uncertain_delta=0.015,
-        )
-        d = t.to_dict()
-        assert d["pixel_uncertain_delta"] == 0.002
-        assert d["lpips_uncertain_delta"] == 0.015
-        assert d["dists_uncertain_delta"] is None
