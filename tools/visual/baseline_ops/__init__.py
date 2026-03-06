4a1c6ae2c986feb9c199acb3c81e3a9c208f0c49
Michal Pielaszkiewicz <michal.pielaszkiewicz>
2026-03-06 10:45:18 +0100

    wip

--- tools/visual/baseline_ops/__init__.py
+++ tools/visual/baseline_ops/__init__.py
@@ -1,13 +1,19 @@
 from .versioning import (
     apply_version_copy,
+    check_local_vs_minio,
+    clean_local_versions,
     list_local_versions,
     list_minio_versions,
     promote_candidates_local,
+    recreate_from_minio,
 )
 
 __all__ = [
     "apply_version_copy",
+    "check_local_vs_minio",
+    "clean_local_versions",
     "list_local_versions",
     "list_minio_versions",
     "promote_candidates_local",
+    "recreate_from_minio",
 ]
