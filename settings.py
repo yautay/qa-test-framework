a521d7935f3c87ca86ae516875163cadec454775
Michal Pielaszkiewicz <michal.pielaszkiewicz@komputronik.pl>
2026-04-15 11:39:27 +0200

    wip

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Logging
-console_log_level = "WARNING"
+console_log_level = "ERROR"
 console_suppress_reporting_api_logs = True
 tools_file_log_level = "WARNING"
 tools_logs_dir = "tools/logs"
@@ -20,7 +20,7 @@ min_expected_tests = 1
 
 # Target git-info probe
 run_git_info_frontend_endpoint = "/git-info"
-run_git_info_backend_endpoint = "/git-info"
+run_git_info_backend_endpoint = "private-api/git-info"
 run_git_info_timeout_seconds = 3
 
 # Grid
